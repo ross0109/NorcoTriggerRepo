a83376f374152649fe0a5056835e46caa8148101
unknown <RCavalieri@US-507-02.amsacs.org>
2014-04-17 13:36:40 -0400

    Initial Project setup

---
+++ Network/Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	public Transform playerPrefab;
+
+	// Use this for initialization
+	void Start () {
+		if(Network.isServer)
+			Network.Instantiate(playerPrefab, transform.position, transform.rotation, 0);
+	}
+	//only called on clients
+	void OnConnectedToServer(){
+		Network.Instantiate(playerPrefab, transform.position, transform.rotation, 0);
+	}
+	//only called on the server to remove all of the player data from the server
+	void OnPlayerDisconnected(NetworkPlayer player) {
+		Network.RemoveRPCs(player);
+		Network.DestroyPlayerObjects(player);
+	}
+}
---
+++ Network/Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	CharacterController controller;
+	int speed = 5;
+	Vector3 newPos = new Vector3(0, 0, 0);
+
+	// Use this for initialization
+	void Start () {
+		controller = gameObject.GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(networkView.isMine){
+			newPos = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			controller.Move (newPos*speed*Time.deltaTime*2);
+		}
+
+	}
+}
