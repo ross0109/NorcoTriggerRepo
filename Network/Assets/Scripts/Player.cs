6ef6458b27b54ec57dcb4285da52c1ea5b2ddcd2
unknown <RCavalieri@US-507-02.amsacs.org>
2014-05-01 08:27:03 -0400

    no message

--- Network/Assets/Scripts/Player.cs
+++ Network/Assets/Scripts/Player.cs
@@ -5,18 +5,23 @@ public class Player : MonoBehaviour {
 	CharacterController controller;
 	int speed = 5;
 	Vector3 newPos = new Vector3(0, 0, 0);
+	Vector3 lookTarget;
 
-	// Use this for initialization
 	void Start () {
 		controller = gameObject.GetComponent<CharacterController>();
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
 		if(networkView.isMine){
 			newPos = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-			controller.Move (newPos*speed*Time.deltaTime*2);
+			controller.Move (newPos*speed*Time.deltaTime*5);
+			Rotate ();
 		}
-
+	}
+	void Rotate(){
+		Vector3 mousePos = Input.mousePosition;
+		Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+		worldPos.y = transform.position.y;
+		transform.LookAt(worldPos);
 	}
 }
