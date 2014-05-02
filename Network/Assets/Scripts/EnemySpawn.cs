535c39aca69cc7e697e03a109cc73b8b911fa3e7
unknown <RCavalieri@US-507-02.amsacs.org>
2014-05-02 15:02:36 -0400

    random spawning

---
+++ Network/Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawn : MonoBehaviour {
+
+	public GameObject[] spawnObject;
+	public float radius = 1.0f;
+	public float minSpawnTime = 1.0f;
+	public float maxSpawnTime = 10.0f;
+	public bool constantSpawn = false;
+	public Transform spawnPosition;
+	Vector3 randomLocation;
+
+	void Start () {
+		if(constantSpawn == true){
+			SpawnEnemy();
+		}
+		else{
+			Invoke ("SpawnEnemy", Random.Range (minSpawnTime, maxSpawnTime));
+		}
+	}
+
+	void SpawnEnemy () 
+	{
+		randomLocation = Random.insideUnitSphere * 20; //5 is radius
+		randomLocation.y = 0.0f;
+		float spawnRadius = radius;
+		int spawnObjectIndex = Random.Range(0, spawnObject.Length);
+		Instantiate(spawnObject[spawnObjectIndex],spawnPosition.position + randomLocation,spawnObject[spawnObjectIndex].transform.rotation);
+		if (constantSpawn == true)
+		{
+			Invoke ("SpawnEnemy", Random.Range (minSpawnTime,maxSpawnTime));
+		}
+	}
+}
