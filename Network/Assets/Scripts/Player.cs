cbe38062a79296f357eaf9b9f8fbbc1e6f14aeb5
Marcus O'Hara <marcus.ohara1015@gmail.com>
2014-05-02 15:02:08 -0400

    fixed motion

--- Network/Assets/Scripts/Player.cs
+++ Network/Assets/Scripts/Player.cs
@@ -5,7 +5,7 @@ public class Player : MonoBehaviour {
 	CharacterController controller;
 	int speed = 5;
 	Vector3 newPos = new Vector3(0, 0, 0);
-	Vector3 lookTarget;
+
 
 	void Start () {
 		controller = gameObject.GetComponent<CharacterController>();
@@ -15,13 +15,8 @@ public class Player : MonoBehaviour {
 		if(networkView.isMine){
 			newPos = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 			controller.Move (newPos*speed*Time.deltaTime*5);
-			Rotate ();
+			//Rotate ();
 		}
 	}
-	void Rotate(){
-		Vector3 mousePos = Input.mousePosition;
-		Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
-		worldPos.y = transform.position.y;
-		transform.LookAt(worldPos);
-	}
+
 }
---
+++ Network/Assets/Scripts/Rotate.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+	Vector3 lookTarget;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rotate();
+	}
+	void rotate(){
+		Vector3 mousePos = Input.mousePosition;
+		Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+		worldPos.y = transform.position.y;
+		transform.LookAt(worldPos);
+	}
+}
